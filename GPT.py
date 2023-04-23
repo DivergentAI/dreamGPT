93a0ee7588726fd02d8c8c4af138c2bb51654020
artas728 <andrew.volotscov@gmail.com>
2023-04-23 13:25:04 -0700

    init andrii branch

--- GPT.py
+++ GPT.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPEN_AI_API_KEY=os.environ.get('SECRET_KEY')
-
+IDEAS_NUM_PER_CYCLE = 10
 
 def chat_complete(prompt, model="gpt-4", max_tokens=100):
     response = openai.Completion.create(
@@ -18,6 +18,27 @@ def chat_complete(prompt, model="gpt-4", max_tokens=100):
     )
     message = response.choices[0].text.strip()
     return message
+
+def step1():
+    """
+    Generate N new ideas
+    :return:
+    """
+    prompt = f"Generate {IDEAS_NUM_PER_CYCLE} new ideas. Some context {get_context()}"
+    result = chat_complete(prompt)
+
+
+
+def execute_cycle():
+    step1()
+    step2()
+    step3()
+    step4()
+
+def main():
+    while True:
+       execute_cycle()
+
 
 if __name__ == "__main__":
     prompt = "Insert prompt"
