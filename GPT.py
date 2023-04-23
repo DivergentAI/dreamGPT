747abe84762974da0f4b4359260fd8f0c7a16718
Daniel Meilech <melikhov@MacBook-Pro-Daniel.local>
2023-04-23 13:20:26 -0700

    Initialization finalized

--- GPT.py
+++ GPT.py
@@ -1,26 +1,62 @@
 import openai
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-OPEN_AI_API_KEY=os.environ.get('SECRET_KEY')
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-def chat_complete(prompt, model="gpt-4", max_tokens=100):
-    response = openai.Completion.create(
-        engine=model,
-        prompt=prompt,
-        max_tokens=max_tokens,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    message = response.choices[0].text.strip()
-    return message
+def chat_complete(messages ,model="gpt-4"):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages
+)
+    return response
+
+def chat_test():
+    messages = [
+        {"role": "system", "content": "You are a friendly neighbour GPT"},
+        {"role": "user", "content": "Hi, neighbour"},
+    ]
+    print(chat_complete(messages))
+""" Input Message structure
+    [
+        {"role": "system", "content": ""},
+        {"role": "user", "content": ""},
+        {"role": "assistant", "content": ""},
+        {"role": "user", "content": ""}
+    ]"""
+
+""" Output Message structure
+{
+  "choices": [
+    {
+      "finish_reason": "stop",
+      "index": 0,
+      "message": {
+        "content": "Hello! How are you today, neighbour? It's nice to see you. If you need any help or just want to chat, feel free to reach out!",
+        "role": "assistant"
+      }
+    }
+  ],
+  "created": 1682279983,
+  "id": "chatcmpl-78aA3nPI5sCkbD3PjyNDXcUbN79i9",
+  "model": "gpt-4-0314",
+  "object": "chat.completion",
+  "usage": {
+    "completion_tokens": 33,
+    "prompt_tokens": 21,
+    "total_tokens": 54
+  }
+}
+"""
+
+"""[
+    {"title": String,
+    "description": String,
+    "score": float,
+    }
+]"""
+
+
 
 if __name__ == "__main__":
-    prompt = "Insert prompt"
-    response = chat_complete(input())
-    
-    print("Response:", response)
+    chat_test()
